73a08f9fbdddd2888024b2eacbb65e08856d9d75
marloncorreia <malilocsimas26@gmail.com>
2021-04-13 00:50:45 -0300

    added method to delete order by product id

--- models/order.go
+++ models/order.go
@@ -1,6 +1,8 @@
 package models
 
-import "shoppingCart-LI/config"
+import (
+	"shoppingCart-LI/config"
+)
 
 func CreateOrder(productId uint) (Order, error) {
 	var order Order
@@ -25,4 +27,20 @@ func DeleteOrder(order *Order) error {
 	db.Delete(order)
 
 	return nil
+}
+
+func DeleteOrdersByProductId(productId uint) error {
+	db := config.GetConnection()
+
+	var orders []Order
+	db.Find(&orders)
+
+	for _, v := range orders {
+		db.Preload("Product").Find(&v)
+		if v.Product.ID == productId {
+			db.Delete(&v)
+		}
+	}
+	return nil
+
 }
