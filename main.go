b0b1521a0a6f401ec69cfb3a6b47c3669cc4cf00
marloncorreia <malilocsimas26@gmail.com>
2021-04-13 05:00:28 -0300

    initiliaze and migrate db

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-import "shoppingCart-LI/server"
+import (
+	"fmt"
+	"shoppingCart-LI/config"
+	"shoppingCart-LI/models"
+	"shoppingCart-LI/server"
+)
 
 func main() {
 
+	err := config.Init()
+	if err != nil {
+		fmt.Println("status: ", err)
+	}
+
+	schema := []interface{}{
+		&models.User{},
+		&models.Order{},
+		&models.Product{},
+		&models.DiscountCoupon{},
+		&models.Cart{},
+	}
+
+	config.ApllyMigrations(schema)
+
 	server.RunServer()
+
 }
