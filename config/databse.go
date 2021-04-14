812be966027843d3c629949eee54befe57bfe8fe
marloncorreia <malilocsimas26@gmail.com>
2021-04-14 03:46:50 -0300

    use variables from .env file

--- config/databse.go
+++ config/databse.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -8,7 +11,15 @@ import (
 var DB *gorm.DB
 
 func Init() error {
-	dsn := "host=localhost user=marlon password=postgres dbname=postgres port=5432 sslmode=disable"
+	godotenv.Load()
+
+	dsn := "host=" + os.Getenv("DB_HOST") + " " +
+		"user=" + os.Getenv("DB_USER") + " " +
+		"password=" + os.Getenv("DB_PASSWORD") + " " +
+		"dbname=" + os.Getenv("DB_NAME") + " " +
+		"port=" + os.Getenv("DB_PORT") + " " +
+		"sslmode=disable"
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return err
